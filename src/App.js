1c1a1acb560e2e219af90d8f4fc03fbb4b480527
Poojabhcode <poojabhardwaj2099@gmail.com>
2023-11-22 19:03:28 +0530

    Favourites page UI complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Navbar from './Components/Navbar'
-import Banner from './Components/Banner';
-import Movies from './Components/Movies'
+// import Banner from './Components/Banner';
+// import Movies from './Components/Movies';
 import './App.css';
-
+import Favourite  from './Components/Favourite';
 
 function App() {
   return (
     <>
       <Navbar/>
-      <Banner/>
-      <Movies />
-     
+      {/* <Banner/>
+      <Movies /> */}
+      <Favourite/>
 
     </>
   );
