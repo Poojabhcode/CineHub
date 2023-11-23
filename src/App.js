095f61477a8db39efc53dbcfc64fd2b375cc6fe0
Poojabhcode <poojabhardwaj2099@gmail.com>
2023-11-23 13:07:12 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import Navbar from './Components/Navbar'
-// import Banner from './Components/Banner';
-// import Movies from './Components/Movies';
-import './App.css';
+import Banner from './Components/Banner';
+import Movies from './Components/Movies';
 import Favourite  from './Components/Favourite';
+import { BrowserRouter as Router, Switch,Route, Routes} from 'react-router-dom';
+import './App.css';
 
 function App() {
-  return (
-    <>
-      <Navbar/>
-      {/* <Banner/>
-      <Movies /> */}
-      <Favourite/>
-
-    </>
+  return (  
+    <Router>
+       <Navbar/>
+          <Routes>
+                <Route path='/' element={<>
+                    <Banner />
+                    <Movies />
+                    </>} />
+                <Route path='/favourites' element={<Favourite/>} exact/>
+          </Routes>
+    </Router>
   );
 }
 
