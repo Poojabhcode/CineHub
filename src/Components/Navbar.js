78bc78047872c66a0fa4578323810ef3db5dc6a5
Poojabhcode <poojabhardwaj2099@gmail.com>
2023-11-28 16:19:35 +0530

    UI color

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,8 +5,8 @@ export default class Navbar extends Component {
   render() {
     return (
       <div style = {{display:'flex',padding:'0.5'}}>
-        <Link to='/' style={{textDecoration:'none'}} > <h1 style={{marginTop:'1rem',marginLeft:'1rem'}}><i class="fa-solid fa-film"></i>CineHub</h1></Link>
-        <Link to='/favourites' style={{textDecoration:'none'}}><h2 style={{marginLeft:'2rem',marginTop:'1.5rem'}}><i class="fa-brands fa-gratipay"></i>Favourites</h2></Link>
+        <Link to='/' style={{textDecoration:'none'}} > <h1 className='h1' style={{marginTop:'1rem',marginLeft:'1rem'}}><i class="fa-solid fa-film"></i>CineHub</h1></Link>
+        <Link to='/favourites' style={{textDecoration:'none'}}><h2 className='h2' style={{marginLeft:'2rem',marginTop:'1.5rem'}}><i class="fa-brands fa-gratipay"></i>Favourites</h2></Link>
       </div>
     )
   }
